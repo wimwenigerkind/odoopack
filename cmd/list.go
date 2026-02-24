73d84676cb59040b0e1647b26d736e62ad4265a4
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-02-24 10:47:14 +0100

    feat: add list command to list installed addons from manifest

--- cmd/list.go
+++ cmd/list.go
@@ -5,35 +5,35 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/wimwenigerkind/odoopack/pkg/manifest"
 )
 
-// listCmd represents the list command
 var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "list",
+	Short:   "List required addons",
+	Aliases: []string{"l"},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		m, err := manifest.Load()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if len(m.Require) == 0 {
+			fmt.Println("No addons installed")
+			return
+		}
+
+		fmt.Println("Installed")
+		for name, version := range m.Require {
+			fmt.Println("-", name+"@"+version)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
